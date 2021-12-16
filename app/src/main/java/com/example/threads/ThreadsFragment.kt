4ef20f05779b9eaa9d5842b834fb18c9d5b6ca2f
татьяна <emuart155@gmail.com>
2021-12-16 13:29:31 +0300

    Расчет в отдельном потоке. Но значения выводим в UI потоке с помощью view.post(Runnable)

--- app/src/main/java/com/example/threads/ThreadsFragment.kt
+++ app/src/main/java/com/example/threads/ThreadsFragment.kt
@@ -2,6 +2,8 @@ package com.example.threads
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -77,6 +79,54 @@ class ThreadsFragment : Fragment() {
                     })
                 }
             }.start()
+        }
+
+        /**
+         * Кнопка HandlerThread.
+         * Создаем handlerThread и вызываем start
+         * Создаем handler и ложим в него handlerThread.looper
+         */
+        val handlerThread = HandlerThread(getString(R.string.my_handler_thread))
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
+
+        binding.calcThreadHandler.setOnClickListener {
+            binding.mainContainer.addView(AppCompatTextView(it.context).apply {
+                /**
+                 * Добавляется AppCompatTextView - надпись внизу:
+                 * Находимся в потоке: My_Handler_Thread
+                 */
+                text = String.format(
+                    getString(R.string.calculate_in_thread),
+                    handlerThread.name
+                )
+                textSize = resources.getDimension(R.dimen.main_container_text_size)
+            })
+            /**
+             * Handler.post(Runnable) запоминает поток, в котором создан Handler(в UI потоке),
+             * после чего метод run() будет вызываться в этом потоке.
+             * Т .е . считаем в отдельном потоке, а выводим строки в майн потоке.
+             */
+            handler.post {
+                startCalculations(binding.editText.text.toString().toInt())
+                /**
+                 * View.post(Runnable) ставит метод run() в очередь UI-потока.
+                 */
+                binding.mainContainer.post {
+                    binding.mainContainer.addView(AppCompatTextView(it.context).apply {
+                        /**
+                         * Добавляется AppCompatTextView - надпись внизу:
+                         * Считаем в потоке main
+                         */
+                        text = String.format(
+                            getString(R.string.in_thread),
+                            Thread.currentThread().name
+                        )
+                        textSize =
+                            resources.getDimension(R.dimen.main_container_text_size)
+                    })
+                }
+            }
         }
     }
 
